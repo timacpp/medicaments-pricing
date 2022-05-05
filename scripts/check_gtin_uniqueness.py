f3cc91b3469e5bef84a3ce6c1eeb82a6855c1df7
Tima <timathevedmed@gmail.com>
2022-05-05 22:08:13 +0200

    Add script to check GTIN uniqueness

---
+++ scripts/check_gtin_uniqueness.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+if __name__ == '__main__':
+    # Create dictionary like {1: {'gtin': 5909990244713}, ...}
+    data = pd.read_excel(
+        'doc.xlsx',
+        index_col=0,            # Use first column as indices
+        header=1,               # Ignore first row
+        usecols='A,E',          # Select LP and GTIN
+        names=['idx', 'gtin']   # Rename columns
+    ).to_dict('index')
+
+    # Reverse the mapping to gtin -> list of id's
+    mapping = dict()
+    for idx, columns in data.items():
+        mapping.setdefault(columns['gtin'], []).append(idx)
+
+    # Print entries with non-unique gtin
+    for gtin, ids in mapping.items():
+        if len(ids) > 1:
+            print(f'GTIN {gtin} corresponds to: {ids}')
