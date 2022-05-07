d81749ec4c4647ac77416bd9092d4d69cf9224d3
Tima <timathevedmed@gmail.com>
2022-05-07 23:12:55 +0200

    Initialize database tables

--- app/database/initialize.js
+++ app/database/initialize.js
@@ -19,19 +19,56 @@ db.connect((err) => {
 
 db.query('CREATE DATABASE MIMED', (err, result) => {
     if (err) {
-        console.error('Failed to create database');
+        console.error('Failed to create database.');
         throw err;
     }
-    console.log('Database created successfully');
+    console.log('Database created successfully.');
 });
 
-function tableCallback(name, err, result) {
+db.changeUser({database : 'MIMED'}, (err) => {
     if (err) {
+        console.error('Failed to change database.');
+        throw err;
+    }
+    console.log('Database changed successfully.');
+})
+
+function tableCallback(name, err) {
+    if (err) {
+        console.log(`Failed to create table [${name}]`);
         throw err;
     }
 
-    console.log(result);
+    console.log(`Table [${name}] created successfully.`);
 };
+
+db.query(
+    `CREATE TABLE Substancja (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        nazwa TEXT NOT NULL
+    )`
+, (err) => tableCallback('Substancja', err));
+
+db.query(
+    `CREATE TABLE Lek (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        substancja INT NOT NULL ,
+        nazwa TEXT NOT NULL,
+        zawartosc TEXT NOT NULL,
+        FOREIGN KEY (substancja) REFERENCES Substancja(id)
+    )`
+, (err) => tableCallback('Lek', err));
+
+db.query(
+    `CREATE TABLE Cena (
+        wartosc INT NOT NULL,
+        dzien DATE NOT NULL,
+        lek INT NOT NULL,
+        PRIMARY KEY(dzien, lek),
+        FOREIGN KEY(lek) REFERENCES Lek(id)
+    )`
+, (err) => tableCallback('Cena', err));
+
 
 setTimeout(() => {
     console.log('Press Ctrl+C to exit.');
