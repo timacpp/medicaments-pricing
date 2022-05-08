c9f2e04b689fc3b61449f7f7c33384c63203c048
Tima <timathevedmed@gmail.com>
2022-05-08 12:49:08 +0200

    Create example application

--- app/app.js
+++ app/app.js
@@ -3,13 +3,37 @@ const connection = require('./database/connection')
 
 const port = 8080;
 const app = express();
-const db = connection.db
+const db = connection.connection
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+app.use(express.static('static'));
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
 
 app.get('/', (req, res) => {
-    res.send('Hejka');
-});
+    const substance = 'Acarbosum';
+    const sql = `
+    SELECT lek.nazwa
+        FROM Lek lek
+        LEFT JOIN Substancja sub
+        ON sub.nazwa = ?
+        LIMIT 10
+    `;
 
+    db.query(sql, substance, (err, result) => {
+        if (err) {
+            console.error('Query failed.');
+            throw err;
+        }
+
+        names = ''
+        result.forEach(record => {
+            names += record['nazwa'] + ' ';
+        });
+
+        res.render('example.pug', {medicine: names});
+    });
+});
