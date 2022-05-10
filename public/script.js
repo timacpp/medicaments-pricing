154b118f02eb4901fbdcb6e0a6a524d8d4ca81a1
janossowski <jo429579@students.mimuw.edu.pl>
2022-05-10 23:36:23 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -17,48 +17,84 @@ async function buildChart() {
 
     // Represent data as tuples (drug, date, price)
     const records = await response.json();
-
     const dates = [];
     const drugs = [];
     const prices = [];
+    const datasets = [];
     
     for (let i = 0; i < selectedIds.length; i++) {
         prices.push(new Array());
     }
 
-    for (const record of records) {
-        if (!dates.includes(record[1])) {
-            dates.push(record[1]);
-        }
+    var maxDate = 0;
+    var minDate = 9999;
 
-        if (!drugs.includes(record[0])) {
-            drugs.push(record[0]);
-        }
+    for (const record of records) {
+        if(drugs[record[0]] === undefined) drugs[record[0]] = {data: []};
+        drugs[record[0]].data.push({date: record[1], price: record[2]});
+
+        const splitDate = record[1].split("/");
+        const intDate = parseInt(splitDate[0]) + parseInt(splitDate[1]) * 100;
+        if (minDate > intDate) minDate = intDate;
+        if (maxDate < intDate) maxDate = intDate;
     }
 
-    let drug_idx = 0;
-    for (const drug of drugs) {
-        let date_idx = 0;
-        for (const record of records) {
-            if (record[0] === drug) {
-                while (record[1] != dates[date_idx]) {
-                    prices[drug_idx].push(null);
-                    date_idx++;
-                }
-                prices[drug_idx].push(record[2]);
-                date_idx++;
+    var maxYear = parseInt(maxDate/100);
+    var maxMonth = maxDate%100;
+    var minYear = parseInt(minDate/100);
+    var minMonth = minDate%100;
+
+    var range = 0;
+    range += maxYear-minYear;
+    range *= 12;
+    range += maxMonth-minMonth;
+
+    
+    let drugCounter = 0;
+
+    for (const [key, drug] of Object.entries(drugs)) {
+        data = new Array(parseInt(range/2));
+        data.fill(null);
+        
+        for (const entry of drug.data) {
+            const splitDate = entry.date.split("/");
+            let index = 0;
+            index += parseInt(splitDate[1]);
+            index -= minYear;
+            index *= 12;
+            index += parseInt(splitDate[0]);
+            index -= minMonth;
+            data[index] = entry.price;
+        }
+        
+
+        const set = {
+            label: key,
+            fill: false,
+            data: data, 
+            yAxisID: "y-axis-"+drugCounter++,
+            borderColor: '#808080',
+            backgroundColor: '#808080'
+        }
+        datasets.push(set);
+    }    
+    console.log(datasets);
+
+    const ctx = document.getElementById('myChart');
+    const lineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: datasets
+        },
+        options: {
+            responsive: true,
+            hoverMode: 'index',
+            stacked: false,
+            title:{
+                display: true,
+                text:'Chart.js Line Chart - Multi Axis'
             }
         }
-        drug_idx++;
-    }
-
-    console.log(prices);
-    const ctx = document.getElementById('myChart');
-    const myChart = new Chart(ctx, {
-         type: 'line',
-         data: {
-             datasets: prices,
-             labels: dates
-         }
-     });
+    });
 }
